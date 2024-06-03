36a7b06e0bd6e3ef51de65a312cb443edf2f3903
Mostafa-Hamdi <MostafaElMarengy11@gmail.com>
2024-06-03 14:10:41 +0300

    this edit form main-repo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import About from '../main-submodule/src/components/About.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <About />
 </template>
 
 <style scoped>
